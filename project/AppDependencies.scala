6f99483852819d39d21c2e8ad812ed9a1f62a847
damien.butterworth <damienbutterworth@hotmail.co.uk>
2025-03-17 14:51:47 +0000

    Address plat-ops automated findings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % "9.7.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % "2.2.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % "9.11.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % "2.5.0",
     "org.typelevel"     %% "cats-core"                 % "2.13.0"
   )
 
@@ -15,9 +15,9 @@ object AppDependencies {
     "org.scalatestplus"     %% "mockito-5-12"            % "3.2.19.0",
     "org.scalacheck"        %% "scalacheck"              % "1.18.1",
     "org.scalatestplus"     %% "scalacheck-1-18"         % "3.2.19.0",
-    "uk.gov.hmrc"           %% "bootstrap-test-play-30"  % "9.7.0",
+    "uk.gov.hmrc"           %% "bootstrap-test-play-30"  % "9.11.0",
     "org.scalatestplus"     %% "scalacheck-1-18"         % "3.2.19.0",
-    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-30" % "2.2.0",
+    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-30" % "2.5.0",
     "com.vladsch.flexmark"   % "flexmark-all"            % "0.64.8",
     "com.github.tomakehurst" % "wiremock-standalone"     % "3.0.1"
   ).map(_ % Test)
